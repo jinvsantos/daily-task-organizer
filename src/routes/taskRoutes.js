f9baa290c8aad0686f4ea990f0961c06ad8882d7
Jean Santos <jeanvictor.san@gmail.com>
2025-08-08 22:03:24 -0300

    feat(routes): definir rotas para tarefas
    - POST /tasks: criar nova tarefa
    - GET /tasks: listar todas as tarefas
    - GET /tasks/:id: buscar tarefa por ID
    - PUT /tasks/:id: atualizar tarefa existente

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,10 +1,10 @@
-// 1) importa o express para criar o router
 const express = require('express');
 const router = express.Router();
 
-// 3) importa só a ação que já existe no controller
-const { createTask } = require('../controllers/taskController');
-// 4) define a rota POST /tasks apontando para o controller
+const { createTask, getAllTasks, getTaskById, updateTask } = require('../controllers/taskController');
 router.post('/tasks', createTask);
-// 5) exporta o router para o server.js conseguir usar
+router.get('/tasks', getAllTasks);
+router.get('/tasks/:id', getTaskById);
+router.put('/tasks/:id', updateTask);
+
 module.exports = router;
